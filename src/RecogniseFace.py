759f2a91e445d617e3cfab76be7ac9366fc65b64
maureen019 <maureen.kpham@gmail.com>
2024-07-18 20:19:13 +1000

    updated gitignore

--- src/RecogniseFace.py
+++ src/RecogniseFace.py
@@ -13,15 +13,21 @@ class RecogniseFace:
     def __init__(self):
         self.faceMatch = False
         self.similarityScore = 0
-        self.referenceImage = cv2.imread('photos/reference.jpg')
+        self.refFront = cv2.imread('photos/ref_Front.jpg')
+        self.refSide = cv2.imread('photos/ref_Side.jpg')
 
-        if self.referenceImage is None:
+        if self.refFront is None or self.refSide is None:
             raise ValueError("Reference image not found. Check the path to 'photos/reference.jpg'")
 
-        self.faceClassifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.frontFaceClassifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-        if self.faceClassifier.empty():
-            raise ValueError("Failed to load Haar Cascade Classifier")
+        self.sideFaceClassifier = cv2.CascadeClassifier('haarcascade_profileface.xml')
+
+        if self.frontFaceClassifier.empty():
+            raise ValueError("Failed to load Haar Cascade Front Face Classifier")
+
+        if self.sideFaceClassifier.empty():
+            raise ValueError("Failed to load Haar Cascade Face Profile classifer")        
 
     def detectFaces(self, img):
         """
@@ -34,9 +40,27 @@ class RecogniseFace:
             The input image with detected faces
         """
         grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = self.faceClassifier.detectMultiScale(grayImage, 1.3, 3)
+        frontFaces = self.frontFaceClassifier.detectMultiScale(grayImage, 1.3, 3) 
+        print(frontFaces)
+        sideFaces = self.sideFaceClassifier.detectMultiScale(grayImage, 1.3, 3)
+        print(sideFaces)
+        
+        faces = []
+        for value in frontFaces:
+            faces.append(value)
+
+        for value in sideFaces:
+            faces.append(value)
+
+        print(faces)
+        
         if len(faces) == 0:
             return img
+
+        #TODO: fix overlapping boxes. prioritise front face box
+        if len(faces) > 4:
+            for (x,y,w,h) in faces[0]:
+                cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0), 2)
         for (x,y,w,h) in faces:
             cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0), 2)
         return img
@@ -50,8 +74,10 @@ class RecogniseFace:
             Returns True if a match is found
             Returns False otherwise
         """
+        #TODO: figure out how to verify using multiple reference images
+        #TODO: get side profile verified
         try:
-            result = df.verify(frame, self.referenceImage, model_name='Facenet512', distance_metric="cosine") # returns a dictionary
+            result = df.verify(frame, self.refFront, model_name='Facenet512', distance_metric="cosine") # returns a dictionary
             if result['verified']: # use value-pair of key 'verified' to return boolean value
                 self.faceMatch = True
                 self.similarityScore = self.calculateSimilarity(result['distance'])
@@ -68,7 +94,6 @@ class RecogniseFace:
             self.faceMatch = False
     
     def calculateSimilarity(self, distance):
-        # TODO: check the maths
         similarity = (1 - distance) * 100
         res = np.round(similarity, 2)
         return res
@@ -80,8 +105,11 @@ class RecogniseFace:
         return self.similarityScore
 
     def getReferenceImage(self):
-        return self.referenceImage
+        return self.refFront, self.refSide
     
-    def setReferenceImage(self, img):
-        self.referenceImage = img
+    def setReferenceImage(self, img, isFront):
+        if isFront:
+            self.refFront = img
+        else:
+            self.refSide = img
         return self
